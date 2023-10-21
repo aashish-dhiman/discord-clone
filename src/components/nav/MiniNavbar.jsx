e267a05e3bac146b5fbe6f3f2bb268a43fdc8449
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-10-22 03:18:11 +0530

    bug: miniNavbar

--- src/components/nav/MiniNavbar.jsx
+++ src/components/nav/MiniNavbar.jsx
@@ -4,7 +4,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { useEffect, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
-import black_logo from "../../assets/logo/black_logo.svg"
+import black_logo from "../../assets/logo/black_logo.svg";
 
 const MiniNavbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -47,15 +47,14 @@ const MiniNavbar = () => {
                 {isOpen && (
                     <div className="absolute top-0 left-0 flex flex-col items-end w-full min-h-screen overflow-scroll">
                         <div className="w-[350px] relative bg-white min-h-screen rounded-xl z-30 p-8 overflow-scroll">
-                            <Link
-                                to="/"
-                                className="flex items-center justify-between"
-                            >
-                                <img
-                                    src={black_logo}
-                                    alt="logo"
-                                    className=" w-[130px] h-full"
-                                />
+                            <div className="flex items-center justify-between">
+                                <Link to="/">
+                                    <img
+                                        src={black_logo}
+                                        alt="logo"
+                                        className=" w-[130px] h-full"
+                                    />
+                                </Link>
                                 <span
                                     onClick={() => {
                                         setIsOpen(!isOpen);
@@ -64,7 +63,7 @@ const MiniNavbar = () => {
                                 >
                                     <CloseIcon />
                                 </span>
-                            </Link>
+                            </div>
                             <ul className="flex flex-col items-start gap-6 mt-16">
                                 {MenuItems.map((item, i) => {
                                     return (
@@ -87,7 +86,7 @@ const MiniNavbar = () => {
                                     );
                                 })}
                             </ul>
-                            <div className="bg-white w-full ">
+                            <div className="">
                                 <a
                                     href="https://discordapp.page.link/?link=https%3A%2F%2Fitunes.apple.com%2Fus%2Fapp%2Fdiscord-chat-for-games%2Fid985746746%3Ffingerprint%3D1165395575823413372.cHT-wuYpWUxRFm-AZTuqxJm1o-g%26attemptId%3D519d33fe-55f1-49c0-868f-41eb50a63867&utm_source=download&apn=com.discord&isi=985746746&ibi=com.hammerandchisel.discord&sd=Your%20place%20to%20talk%20with%20communities%20and%20friends.&efr=1"
                                     className="absolute bottom-10 left-7 w-fit bg-bgBlue text-white py-3 px-8 rounded-[40px] text-[14px] hover:shadow-[0px_6px_12px_2px_rgba(0,0,0,0.3)] transition duration-200 hover:ease-in-out"
